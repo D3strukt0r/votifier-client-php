a1dabf5408d6d19878686f4e0872976253e86a12
BuildTools <unconfigured@null.spigotmc.org>
2015-09-06 22:15:22 +0200

    Added librarys and corrigated code with php-cs-fixer

--- tests/VotifierTest.php
+++ tests/VotifierTest.php
@@ -3,45 +3,45 @@
 class VotifierTest extends PHPUnit_Framework_TestCase
 {
     private $vote = null;
-    
+
     /**
      * Setup the test enviroment.
      */
     public function setUp()
     {
         $this->vote = new BukkitVotifier\Votifier('manuele.ddns.net',
-                                                          '8192',
-                                                          'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiBUsS/82HP7cHpNT21ztgw8fonUACPiPTqVmw8M7o59H8vsTp0SHbOnmcOMeRmPRlzmBjjsV+WO7GNQeAFd8aW/nenRyuSojFZXeR9N6DFZyD7jz4lHZaJX0e4dkCe41kVt74Cki5rmDY4CtQhfkBeoMaf8eG/HjR/dEugDtizxL01vI4qVE+yriwYoUtw8tZkm1kbn0r4LmkeGVtZmPv1AY3FO8n9b3uxlViBmg5EI6NE89L/vldI9gda91hze4OkGn8UKMvSq5h5HuEMaX3428BMvpnGo6yRXmMjCmc0i0rc8Sr9UxbhMXSc55CX0pauF9e6aws8pUW6FmmlTHcQIDAQAB',
-                                                          'D3strukt0r',
-                                                          'VotifierPHP-Test',
-                                                          '127.0.0.1');
+                                   '8192',
+                                   'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiBUsS/82HP7cHpNT21ztgw8fonUACPiPTqVmw8M7o59H8vsTp0SHbOnmcOMeRmPRlzmBjjsV+WO7GNQeAFd8aW/nenRyuSojFZXeR9N6DFZyD7jz4lHZaJX0e4dkCe41kVt74Cki5rmDY4CtQhfkBeoMaf8eG/HjR/dEugDtizxL01vI4qVE+yriwYoUtw8tZkm1kbn0r4LmkeGVtZmPv1AY3FO8n9b3uxlViBmg5EI6NE89L/vldI9gda91hze4OkGn8UKMvSq5h5HuEMaX3428BMvpnGo6yRXmMjCmc0i0rc8Sr9UxbhMXSc55CX0pauF9e6aws8pUW6FmmlTHcQIDAQAB',
+                                   'D3strukt0r',
+                                   'VotifierPHP-Test',
+                                   '127.0.0.1');
     }
-    
-	/**
-	 * Teardown the test environment.
-	 */
-	public function tearDown()
-	{
+
+    /**
+     * Teardown the test environment.
+     */
+    public function tearDown()
+    {
         $this->vote = null;
     }
-    
-	/**
-	 * Test instance of $this->vote
-	 *
-	 * @test
-	 */
-	public function testInstanceOf()
-	{
+
+    /**
+     * Test instance of $this->vote.
+     *
+     * @test
+     */
+    public function testInstanceOf()
+    {
         $this->assertInstanceOf('BukkitVotifier\Votifier', $this->vote);
     }
-    
-	/**
-	 * Test vote return the value true.
-	 *
-	 * @test
-	 */
-	public function testValidResult()
-	{
-		$this->assertEquals(true, $this->vote->send());
-	}
+
+    /**
+     * Test vote return the value true.
+     *
+     * @test
+     */
+    public function testValidResult()
+    {
+        $this->assertEquals(true, $this->vote->send());
+    }
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit327ddd9e32fb32d7ed898918d1310f5b
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit327ddd9e32fb32d7ed898918d1310f5b', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
