72866b0b751b058371bd978e3e90ed67788f3ad5
BuildTools <unconfigured@null.spigotmc.org>
2015-09-21 22:46:35 +0200

    Try to fix travis

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,5 +7,4 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
-    'BukkitVotifier\\' => array($baseDir . '/src/BukkitVotifier'),
 );
