295bf8616c895da4a7dfe2f0f621627c834a2931
Manuele Vaccari <manuele.vaccari@gmail.com>
2018-04-21 00:54:03 +0200

    Update 123

--- tests/Votifier/Client/VoteTest.php
+++ tests/Votifier/Client/VoteTest.php
@@ -16,8 +16,8 @@ class VoteTest extends TestCase
     {
         $this->vote = new Vote(
             'localhost',
-            '8192',
-            'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoTc+zUuPLFASP1+JdYVHkT+nsFxOx+mo8yLPGyPA42w3QAZA3ZNzNXWK01gl9o6G3wmiHGi2lwgGW+3IAbOYuEa+Wv0STHE9IOf+T/tDRg0agjrNnWs7un8LUJL0UvI/hMRbrsPJeWy38PbTLC2Bvl0WhU3IsakUWs3mI6Q1INr924zjeCkngOMRjApiA5IJI0OznU50bNdanA6oIFxy83IBpgA4cnccWBSB2g80LDJOxCfUP7oJGw2VJqyBTR5Xi6lAoSv+9uGMTj99W+Ib6a42fLSe6a2FxmyHi7BU7n9W/LAPYxxmelsE3EIKxWK6UBk6i28fpbMi6S1KVV/6lwIDAQAB',
+            8192,
+            getenv('VOTIFIER_KEY'),
             'D3strukt0r',
             'Votifier-PHP-Client Test',
             '127.0.0.1'
