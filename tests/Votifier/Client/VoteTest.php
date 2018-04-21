f6475d9ed9483b28bdfa350771effc244e7569bd
Manuele Vaccari <manuele.vaccari@gmail.com>
2018-04-21 22:47:03 +0200

    Update

--- tests/Votifier/Client/VoteTest.php
+++ tests/Votifier/Client/VoteTest.php
@@ -3,10 +3,10 @@
 namespace Votifier\Client;
 
 use PHPUnit\Framework\TestCase;
-use Votifier\Client\Vote;
 
 class VoteTest extends TestCase
 {
+    /** @var \Votifier\Client\Vote */
     private $vote = null;
 
     /**
@@ -15,9 +15,9 @@ class VoteTest extends TestCase
     public function setUp()
     {
         $this->vote = new Vote(
-            'localhost',
-            8192,
-            getenv('VOTIFIER_KEY'),
+            getenv('VOTIFIER_HOST') ?: '89.163.242.97',
+            getenv('VOTIFIER_PORT') ?: 35670,
+            getenv('VOTIFIER_KEY') ?: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhQGwChJARqa2/e3v6MLi6QXj+Z0vf8aRuruczNeCF5vXqmZWl2m31fH2bh5yOXodseEonRoV7xIbQEMUqh2fWA1PkglAqOIKawOHxjoDtGWpdi1gaAOrCGDJJxcNH/y5E3oQboQfhbR8FB2w1E0JGlwRho9KWpLSifKXhZwivuymjGaOi65hjifbC+Uw4ESzcUdL9e6xR+pn49/99vX5+duDRhTSAwei6fM/uTW4342iL2S7oJh+Jo1ceLgmmXTBvosQ3J93u1SqFCPYQS9Io8fq7MdyIx27HIqidsEk1N3v0Okn3YLkNp8d+YHJ6tCAcQvtfS7B2TWXCNV9fGwVNwIDAQAB',
             'D3strukt0r',
             'Votifier-PHP-Client Test',
             '127.0.0.1'
