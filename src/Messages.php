700385546f2ffb4bf15a74d28347b5f201a69605
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-03-19 20:48:28 +0100

    Update header text

--- src/Messages.php
+++ src/Messages.php
@@ -6,8 +6,8 @@
  * @package   VotifierClient
  * @author    Manuele Vaccari <manuele.vaccari@gmail.com>
  * @copyright Copyright (c) 2017-2020 Manuele Vaccari <manuele.vaccari@gmail.com>
- * @license   https://github.com/D3strukt0r/Votifier-PHP-Client/blob/master/LICENSE.md MIT License
- * @link      https://github.com/D3strukt0r/Votifier-PHP-Client
+ * @license   https://github.com/D3strukt0r/votifier-client-php/blob/master/LICENSE.txt GNU General Public License v3.0
+ * @link      https://github.com/D3strukt0r/votifier-client-php
  */
 
 namespace D3strukt0r\VotifierClient;
